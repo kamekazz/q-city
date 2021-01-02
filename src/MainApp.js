c6b5d12caa550356a77344210966126a944188d0
kamekazz <taveras78@gmail.com>
2021-01-02 13:13:55 -0500

    fix key confing

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,34 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
 
-
-import React from 'react'
-import { connect } from 'react-redux'
-
-import Sidebar from 'components/Sidebar'
-import Navbar from 'components/Navbar'
-import Routes from './Routes'
-import Spinner from 'components/Spinner'
+import Sidebar from "components/Sidebar";
+import Navbar from "components/Navbar";
+import Routes from "./Routes";
+import Spinner from "components/Spinner";
 
 class MainApp extends React.Component {
-
-  renderApplication = auth => 
-    <React.Fragment>
-      <Navbar 
-        loadFresh
-        id="navbar-main"
-        auth={auth}/>
-      <Navbar 
-        auth={auth}
-        id="navbar-clone" />
+  renderApplication = (auth) => (
+    <>
+      <Navbar loadFresh id="navbar-main" auth={auth} />
+      <Navbar auth={auth} id="navbar-clone" />
       <Sidebar />
       <Routes />
-    </React.Fragment>
-    
+    </>
+  );
+
   render() {
-    const { auth } = this.props
-    return auth.isAuthResolved ? this.renderApplication(auth) : <Spinner />
+    const { auth } = this.props;
+    return auth.isAuthResolved ? this.renderApplication(auth) : <Spinner />;
   }
 }
 
-const mapStateToProps = state => ({auth: state.auth})
-  
-export default connect(mapStateToProps)(MainApp)
+const mapStateToProps = (state) => ({ auth: state.auth });
+
+export default connect(mapStateToProps)(MainApp);
--- src/api/auth.js
+++ src/api/auth.js
@@ -1,39 +1,46 @@
+import firebase from "firebase/app";
+import "firebase/auth";
 
+import db from "db";
 
-import firebase from 'firebase/app'
-import 'firebase/auth'
+const createUserProfile = (userProfile) =>
+  db.collection("profiles").doc(userProfile.uid).set(userProfile);
 
-import db from 'db'
-
-const createUserProfile = (userProfile) => 
-  db.collection('profiles')
-    .doc(userProfile.uid)
-    .set(userProfile)
-
-export const register = async ({email, password, fullName, avatar}) => {
+export const register = async ({ email, password, fullName, avatar }) => {
   try {
-    const res = await firebase.auth().createUserWithEmailAndPassword(email, password)
-    const { user } = res
-    const userProfile = { uid: user.uid, fullName, email, avatar, services: [], description: ''}
-    await createUserProfile(userProfile)
-    return userProfile
-  } catch(error) {
-    return Promise.reject(error.message)
+    const res = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+    const { user } = res;
+    const userProfile = {
+      uid: user.uid,
+      fullName,
+      email,
+      avatar,
+      services: [],
+      description: "",
+    };
+    await createUserProfile(userProfile);
+    return userProfile;
+  } catch (error) {
+    return Promise.reject(error.message);
   }
-}
+};
 
-export const login = ({email, password}) => 
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch(error => Promise.reject(error.message))
+export const login = ({ email, password }) =>
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => Promise.reject(error.message));
 
-export const logout = () => firebase.auth().signOut()
+export const logout = () => firebase.auth().signOut();
 
-export const onAuthStateChanged = onAuthCallback => 
-  firebase.auth().onAuthStateChanged(onAuthCallback)
+export const onAuthStateChanged = (onAuthCallback) =>
+  firebase.auth().onAuthStateChanged(onAuthCallback);
 
-
-export const getUserProfile = uid =>
-  db.collection('profiles')
+export const getUserProfile = (uid) =>
+  db
+    .collection("profiles")
     .doc(uid)
     .get()
-    .then(snapshot => ({uid, ...snapshot.data()}))
+    .then((snapshot) => ({ uid, ...snapshot.data() }));
