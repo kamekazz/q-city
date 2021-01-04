28be578cb5ea038f19d5bc8f66763477dd70de9f
kamekazz <taveras78@gmail.com>
2021-01-03 20:28:09 -0500

    start validation

--- src/pages/CreateIssue/MultiForm.js
+++ src/pages/CreateIssue/MultiForm.js
@@ -6,10 +6,68 @@ import {
   Button,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-const MultiForm = () => {
-  const getSteps = () => {};
+import { useState } from "react";
+import ProductInfo from "./ProductInfo";
 
-  return <div>hola MultiForm</div>;
+const useStyles = makeStyles({
+  root: {
+    width: "50%",
+    margin: "1rem auto",
+    border: "1px solid #999",
+    "& .MuiStepIcon-active": {
+      color: "blue",
+    },
+    "& .MuiStepIcon-completed": {
+      color: "green",
+    },
+  },
+});
+
+const MultiForm = () => {
+  const getSteps = () => {
+    return ["SIGN UP", "CHOOSE PLAN", "CHECK OUT"];
+  };
+
+  const [activateStep, setStep] = useState(0);
+
+  const getStepsContent = (_stepIndex) => {
+    switch (_stepIndex) {
+      case 0:
+        return <ProductInfo />;
+      case 1:
+        return "2 Choose plan";
+      case 2:
+        return "3 Check out";
+      default:
+        return "undone step";
+    }
+  };
+  const handelStep = () => {
+    setStep((_state) => _state + 1);
+  };
+  const steps = getSteps();
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Stepper alternativeLabel activeStep={activateStep}>
+        {steps.map((_label) => (
+          <Step key={_label}>
+            <StepLabel>{_label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      {activateStep === steps.length ? (
+        "The Step completed"
+      ) : (
+        <>
+          {getStepsContent(activateStep)}
+          <Button onClick={handelStep}>
+            {activateStep === steps.length ? "Finch" : "Next"}
+          </Button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MultiForm;
---
+++ src/pages/CreateIssue/ProductInfo.js
@@ -0,0 +1,21 @@
+import {
+  Typography,
+  Button,
+  Grid,
+  Checkbox,
+  TextField,
+  OutlinedInput,
+  FormControl,
+  InputLabel,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const ProductInfo = () => {
+  return (
+    <div>
+      <Typography variant="h5"></Typography>
+    </div>
+  );
+};
+
+export default ProductInfo;
