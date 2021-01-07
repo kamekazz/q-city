5bfa01704801f7d8e8c2ababde24df3935ebd8a4
kamekazz <taveras78@gmail.com>
2021-01-07 00:49:24 -0500

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,84 +1,136 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
 
-import React, { useState } from 'react'
-import {useForm} from 'react-hook-form'
-import { useToasts } from 'react-toast-notifications'
-import { Redirect } from 'react-router-dom'
-import onlyGuest from 'components/hoc/onlyGuest'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useToasts } from "react-toast-notifications";
+import { Redirect } from "react-router-dom";
+import onlyGuest from "components/hoc/onlyGuest";
+import { login } from "Redux/actions";
 
-import { login } from 'Redux/actions'
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Login = () => {
-  const [ redirect, setRedirect ] = useState(false)
-  const { register, handleSubmit } = useForm()
-  const { addToast } = useToasts()
+  const classes = useStyles();
+  const [redirect, setRedirect] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const { addToast } = useToasts();
 
-  const onLogin = loginData => {
-    login(loginData)
-      .then(
-        _ => setRedirect(true),
-        errorMessage => addToast(errorMessage, { appearance: 'error', autoDismiss: true, autoDismissTimeout: 3000 })
-      )
+  const onLogin = (loginData) => {
+    console.log("start");
+    login(loginData).then(
+      (_) => setRedirect(true),
+      (errorMessage) =>
+        addToast(errorMessage, {
+          appearance: "error",
+          autoDismiss: true,
+          autoDismissTimeout: 3000,
+        })
+    );
+  };
+
+  if (redirect) {
+    return <Redirect to="/" />;
   }
 
-  if (redirect) { return <Redirect to="/" />}
-
   return (
-    <div className="auth-page">
-      <div className="container has-text-centered">
-        <div className="column is-4 is-offset-4">
-          <h3 className="title has-text-grey">Login</h3>
-          <p className="subtitle has-text-grey">Please login to proceed.</p>
-          <div className="box">
-            <figure className="avatar">
-              <img src="https://placehold.it/128x128" alt="Company Logo" />
-            </figure>
-            <form onSubmit={handleSubmit(onLogin)}>
-              <div className="field">
-                <div className="control">
-                  <input 
-                    ref={register}
-                    name="email"
-                    className="input is-large"
-                    type="email"
-                    placeholder="Your Email"
-                    autoComplete="email" />
-                </div>
-              </div>
-              <div className="field">
-                <div className="control">
-                  <input 
-                    ref={register}
-                    name="password"
-                    className="input is-large"
-                    type="password"
-                    placeholder="Your Password"
-                    autoComplete="current-password" />
-                </div>
-              </div>
-              <button
-                type="submit"
-                className="button is-block is-info is-large is-fullwidth">Sign In</button>
-            </form>
-          </div>
-          <p className="has-text-grey">
-            <a>Sign In With Google</a>&nbsp;
-            <a href="/">Sign Up</a> &nbsp;·&nbsp;
-            <a href="../">Need Help?</a>
-          </p>
-        </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form
+          className={classes.form}
+          noValidate
+          onSubmit={handleSubmit(onLogin)}
+        >
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            inputRef={register({ required: true })}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            inputRef={register({ required: true })}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
       </div>
-    </div>
-  )
-}
+    </Container>
+  );
+};
 
-export default onlyGuest(Login)
-
-
-
-
-
-
-
-
-
+export default onlyGuest(Login);
