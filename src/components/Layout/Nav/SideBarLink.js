8db2cd3aea47732b4dd468fafa29f6d154f9d2fa
kamekazz <taveras78@gmail.com>
2021-01-07 19:50:25 -0500

    cleand

--- src/components/Layout/Nav/SideBarLink.js
+++ src/components/Layout/Nav/SideBarLink.js
@@ -1,9 +1,7 @@
-import InboxIcon from "@material-ui/icons/MoveToInbox";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import MailIcon from "@material-ui/icons/Mail";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
