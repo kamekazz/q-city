6d7668e3bf84ea57d34a5eb082a8ee154f6fdc99
kamekazz <taveras78@gmail.com>
2021-01-13 20:58:44 -0500

    start de udeme clase

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import Navbar from "components/Layout/Nav/Navbar";
-import Routes from "./Routes";
-import Spinner from "components/Spinner";
+import Navbar from 'components/Layout/Nav/Header';
+import Routes from './Routes';
+import Spinner from 'components/Spinner';
 
 class MainApp extends React.Component {
   renderApplication = (auth) => (
-    <>
-      <Navbar loadFresh id="navbar-main" auth={auth}>
-        <Routes />
-      </Navbar>
-    </>
+    <Navbar loadFresh id="navbar-main" auth={auth}>
+      <Routes />
+    </Navbar>
   );
 
   render() {
