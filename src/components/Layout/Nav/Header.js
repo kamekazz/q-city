672251b08026de5d364be449f6b69325b7db155c
kamekazz <taveras78@gmail.com>
2021-01-13 21:40:36 -0500

    add new header

--- src/components/Layout/Nav/Header.js
+++ src/components/Layout/Nav/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-
+import PropTypes from 'prop-types';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -14,13 +14,32 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import ProfileMenu from './ProfileMenu';
 import SideBarLink from './SideBarLink';
 import ReportProblemTwoToneIcon from '@material-ui/icons/ReportProblemTwoTone';
-const drawerWidth = 240;
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
-function Header() {
+function ElevationScroll(props) {
+  const { children, window } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 275,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+function Header(props) {
   return (
-    <AppBar>
-      <Toolbar>Q-City</Toolbar>
-    </AppBar>
+    <ElevationScroll {...props}>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">Q-City</Typography>
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
   );
 }
 
