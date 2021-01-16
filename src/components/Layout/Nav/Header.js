7e7a8ab067793dbf7f248ccec8e66b8c064af0d0
kamekazz <taveras78@gmail.com>
2021-01-16 18:29:41 -0500

    setAction tab

--- src/components/Layout/Nav/Header.js
+++ src/components/Layout/Nav/Header.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import { Tab, Tabs, Toolbar, Button } from '@material-ui/core';
+import { Tab, Tabs, Toolbar, Button, Menu, MenuItem } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import logo from 'assets/logo.svg';
@@ -20,14 +20,26 @@ function ElevationScroll(props) {
 
 function Header(props) {
   const classes = useStyles();
+  let urlLocation = useLocation().pathname;
   const [value, setValue] = useState(0);
   const history = useHistory();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   const handleNavigateTo = (_urlRoute) => {
     history.push(_urlRoute);
+  };
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOpen(false);
   };
 
   return (
@@ -53,30 +65,35 @@ function Header(props) {
                 _to="/"
                 _label="Home"
                 _value={0}
+                _urlLocation={urlLocation}
+                _setValue={setValue}
+              />
+              <MultiMenuTap
+                _to="/services"
+                _label="Services"
+                _value={1}
+                _urlLocation={urlLocation}
                 _setValue={setValue}
               />
               <ActionTab
                 _to="/create_issue"
                 _label="issue"
-                _value={1}
-                _setValue={setValue}
-              />
-              <ActionTab
-                _to="/the_revolution"
-                _label="The Revolution"
                 _value={2}
+                _urlLocation={urlLocation}
                 _setValue={setValue}
               />
               <ActionTab
                 _to="/about_us"
                 _label="About Us"
-                _value={3}
+                _value={4}
+                _urlLocation={urlLocation}
                 _setValue={setValue}
               />
               <ActionTab
                 _to="/contact_us"
                 _label="Contact Us"
-                _value={4}
+                _value={5}
+                _urlLocation={urlLocation}
                 _setValue={setValue}
               />
             </Tabs>
@@ -127,13 +144,58 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ActionTab = (props) => {
-  let { _to, _label, _setValue, _value } = props;
+  let { _to, _label, _setValue, _value, _urlLocation } = props;
   const classes = useStyles();
-  let urlLocation = useLocation().pathname;
-  if (urlLocation === _to) {
-    _setValue(_value);
-  }
+  useEffect(() => {
+    if (_urlLocation === _to) {
+      _setValue(_value);
+    }
+  }, [_setValue, _urlLocation]);
+
   return (
     <Tab component={Link} className={classes.tab} to={_to} label={_label} />
+  );
+};
+
+const MultiMenuTap = (props) => {
+  let { _to, _label, _setValue, _value, _urlLocation } = props;
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = useState(false);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  useEffect(() => {
+    if (_urlLocation === _to) {
+      _setValue(_value);
+    }
+  }, [_setValue, _urlLocation]);
+
+  return (
+    <>
+      <Tab
+        component={Link}
+        className={classes.tab}
+        to={_to}
+        label={_label}
+        onMouseOver={handleClick}
+      />
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </>
   );
 };
