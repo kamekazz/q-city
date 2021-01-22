99b5481bf9ab6ec3e7b1901338100ee22f67cc34
kamekazz <taveras78@gmail.com>
2021-01-21 19:18:23 -0500

    change the color on the delete button to red

--- src/pages/CreateIssue/DeleteModel.js
+++ src/pages/CreateIssue/DeleteModel.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import { Button } from "@material-ui/core";
-import { deleteReport } from "api/report";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+import { deleteReport } from 'api/report';
+import { useHistory } from 'react-router-dom';
 
 function getModalStyle() {
   const top = 50;
@@ -18,12 +18,16 @@ function getModalStyle() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: "absolute",
+    position: 'absolute',
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
+    border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
+  },
+  deleteButton: {
+    backgroundColor: theme.palette.error.main,
+    color: 'white',
   },
 }));
 
@@ -49,18 +53,18 @@ export default function DeleteModal(props) {
 
   const handleSave = () => {
     handleClose();
-    history.push("/home");
+    history.push('/home');
   };
 
   const body = (
     <div style={modalStyle} className={classes.paper}>
       <h2 id="simple-modal-title">would you like to continue?</h2>
       <p id="simple-modal-description">
-        if ypu decide to save you can come back and finish a report at any time
+        if you decide to save you can come back and finish a report at any time
       </p>
       <Button
         variant="contained"
-        color="secondary"
+        className={classes.deleteButton}
         style={{ marginRight: 6 }}
         onClick={handleDelete}
       >
