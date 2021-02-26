4094ed83f85af7864ce7325340b733ffc4ba0df3
kamekazz <taveras78@gmail.com>
2021-02-25 22:23:53 -0500

    done landeng UI

--- src/pages/GunTran/Components/ActiveUsers.jsx
+++ src/pages/GunTran/Components/ActiveUsers.jsx
@@ -1,6 +1,50 @@
 import { Paper } from '@material-ui/core';
 import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import { theme } from 'styles/muiTheme';
 
+const myState = {
+  series: [
+    {
+      name: 'RowD',
+      data: Array.from({ length: 20 }, () => Math.floor(Math.random() * 2)),
+    },
+    {
+      name: 'RowC',
+      data: Array.from({ length: 20 }, () => Math.floor(Math.random() * 2)),
+    },
+    {
+      name: 'RowB',
+      data: Array.from({ length: 20 }, () => Math.floor(Math.random() * 2)),
+    },
+    {
+      name: 'RowA',
+      data: Array.from({ length: 20 }, () => Math.floor(Math.random() * 2)),
+    },
+  ],
+  options: {
+    chart: {
+      height: 350,
+      type: 'heatmap',
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    colors: [theme.palette.primary.main, theme.palette.secondary.main],
+    title: {
+      text: 'Active Users',
+    },
+  },
+};
 export default function ActiveUsers() {
-  return <Paper>ActiveUsers</Paper>;
+  return (
+    <Paper>
+      <ReactApexChart
+        options={myState.options}
+        series={myState.series}
+        type="heatmap"
+        height={200}
+      />
+    </Paper>
+  );
 }
--- src/pages/GunTran/Components/BatteryCount.jsx
+++ src/pages/GunTran/Components/BatteryCount.jsx
@@ -1,6 +1,92 @@
-import { Paper } from '@material-ui/core';
+import { makeStyles, Paper, Typography } from '@material-ui/core';
 import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import { theme } from 'styles/muiTheme';
+const myState = {
+  series: [45],
+  options: {
+    chart: {
+      type: 'radialBar',
+      offsetY: -20,
+      sparkline: {
+        enabled: true,
+      },
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -90,
+        endAngle: 90,
+        track: {
+          background: '#e7e7e7',
+          strokeWidth: '97%',
+          margin: 5, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            color: '#999',
+            opacity: 1,
+            blur: 2,
+          },
+        },
+        dataLabels: {
+          name: {
+            show: false,
+          },
+          value: {
+            offsetY: -2,
+            fontSize: '22px',
+          },
+        },
+      },
+    },
+    grid: {
+      padding: {
+        top: -10,
+      },
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'light',
+        shadeIntensity: 0.4,
+        inverseColors: false,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 50, 53, 91],
+      },
+    },
+    labels: ['Average Results'],
+  },
+};
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+}));
 export default function BatteryCount() {
-  return <Paper>battery count </Paper>;
+  const classes = useStyles();
+  return (
+    <Paper className={classes.root}>
+      <Typography style={{ fontSize: 22 }}>Available Batteries</Typography>
+      <ReactApexChart
+        options={myState.options}
+        series={myState.series}
+        type="radialBar"
+      />
+      <Typography
+        style={{
+          fontSize: 45,
+          fontWeight: 'bold',
+          color: theme.palette.primary.main,
+        }}
+      >
+        50/120
+      </Typography>
+    </Paper>
+  );
 }
