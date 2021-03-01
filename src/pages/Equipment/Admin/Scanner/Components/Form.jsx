fc772e6f4e6cf71f577e6dbd34c10ee2d0f845a8
kamekazz <taveras78@gmail.com>
2021-03-01 00:32:18 -0500

    form ui done

--- src/pages/Equipment/Admin/Scanner/Components/Form.jsx
+++ src/pages/Equipment/Admin/Scanner/Components/Form.jsx
@@ -1,35 +1,126 @@
-import { makeStyles, Paper, TextField, Typography } from '@material-ui/core';
+import {
+  Button,
+  makeStyles,
+  Paper,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 import React from 'react';
 import Switch from '@material-ui/core/Switch';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 const useStyles = makeStyles((theme) => ({
   root: {
     gridArea: 'Form',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '1rem',
+    justifyContent: 'space-between',
+    // maxHeight: 322,
+  },
+  h1: { fontSize: 30 },
+  switchContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: '1rem',
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  assignedButton: {
+    backgroundColor: theme.palette.success.main,
+    color: 'white',
+    '&:hover': {
+      backgroundColor: theme.palette.success.dark,
+    },
+  },
+  deleteButton: {
+    color: theme.palette.error.main,
+    '&:hover': {
+      backgroundColor: theme.palette.error.light,
+      color: 'white',
+    },
+  },
+  formControl: {
+    marginBottom: '1rem',
   },
 }));
-export default function Form() {
+export default function Form({ isEdit = false }) {
   const classes = useStyles();
   const [state, setState] = React.useState({
     checkedA: true,
     checkedB: true,
   });
+  const [age, setAge] = React.useState('');
+  const handleChangeAge = (event) => {
+    setAge(event.target.value);
+  };
 
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
   return (
-    <Paper className={classes.root}>
-      <Typography>Add New Scanner</Typography>
+    <Paper className={classes.root} style={{ maxHeight: isEdit ? 425 : 322 }}>
+      <Typography className={classes.h1}>
+        {isEdit ? 'Edit Scanner' : 'Add New Scanner'}
+      </Typography>
       <TextField label="Number ID" />
       <TextField label="Bar-Code" />
       <TextField label="Serial Number" />
-      <Typography>Ready for Use</Typography>
-      <Switch
-        checked={state.checkedB}
-        onChange={handleChange}
-        color="secondary"
-        name="checkedB"
-        inputProps={{ 'aria-label': 'secondary  checkbox' }}
-      />
+
+      {isEdit ? (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Status</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={handleChangeAge}
+            >
+              <MenuItem value={'it hold'}>IT Hold</MenuItem>
+              <MenuItem value={'check scanner'}>Check Scanner</MenuItem>
+              <MenuItem value={'ready for'}>Ready for Use</MenuItem>
+            </Select>
+          </FormControl>
+          <div className={classes.switchContainer}>
+            <Button variant="contained" color="primary">
+              History
+            </Button>
+            <Button variant="contained" className={classes.assignedButton}>
+              return
+            </Button>
+          </div>
+          <div className={classes.switchContainer}>
+            <Button className={classes.assignedButton}>assigned to</Button>
+            <Button variant="contained" color="secondary">
+              release
+            </Button>
+          </div>
+        </>
+      ) : (
+        <div className={classes.switchContainer}>
+          <Typography component="h3">Ready for Use</Typography>
+          <Switch
+            checked={state.checkedB}
+            onChange={handleChange}
+            color="secondary"
+            name="checkedB"
+            inputProps={{ 'aria-label': 'secondary  checkbox' }}
+          />
+        </div>
+      )}
+
+      <div className={classes.buttonContainer}>
+        <Button>cancel</Button>
+        {isEdit && <Button className={classes.deleteButton}>delete</Button>}
+        <Button color="primary">Save</Button>
+      </div>
     </Paper>
   );
 }
--- src/pages/Equipment/Admin/Scanner/Components/Table.jsx
+++ src/pages/Equipment/Admin/Scanner/Components/Table.jsx
@@ -10,7 +10,7 @@ import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles({
   root: {
-    gridArea: 'Table',
+    gridArea: 'Form',
   },
   table: {
     minWidth: 650,
@@ -22,11 +22,11 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('600', 'manuel taveras', 6.0, 24, 4.0),
+  createData('601', 'tom hack', 9.0, 37, 4.3),
+  createData('602', 'kendra uceta', 16.0, 24, 6.0),
+  createData('603', 'jason water', 3.7, 67, 4.3),
+  createData('604', 'kara welch', 16.0, 49, 3.9),
 ];
 
 export default function BasicTable() {
@@ -37,11 +37,11 @@ export default function BasicTable() {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell>User</TableCell>
+            <TableCell>status</TableCell>
+            <TableCell>Active</TableCell>
+            <TableCell>Date</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -50,10 +50,10 @@ export default function BasicTable() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell>{row.calories}</TableCell>
+              <TableCell>{row.fat}</TableCell>
+              <TableCell>{row.protein}</TableCell>
+              <TableCell>{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
